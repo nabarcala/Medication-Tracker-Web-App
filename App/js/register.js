a56eda83aa351c7740eba9719833c7b4e2bc2811
Natacha <nabarcala@gmail.com>
2019-04-08 20:09:51 -0400

    Created password check script

--- App/js/register.js
+++ App/js/register.js
@@ -1,1 +1,14 @@
 $(document).ready(function() {});
+
+function checkPassword(form){
+    password = form.inputPassword.value;
+    confirmPassword = form.confirmPassword.value;
+    
+    if ( password != confirmPassword){
+        alert("\nThe passwords do not match. Try again.")
+        return false;
+    }
+    else{
+        return true;
+    }
+}
